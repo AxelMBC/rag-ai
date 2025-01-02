2b241b475e9894e4b1ab969be7a357f1fa85de09
3ARR4 <barrazaaxel2000@gmail.com>
2025-01-01 19:30:41 -0700

    styling improv

--- app/auth/sign-in/signin.tsx
+++ app/auth/sign-in/signin.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import { handleGoogleSignIn } from "@/app/lib/auth/googleSignInServerAction";
 import "./styles.scss";
 import { FcGoogle } from "react-icons/fc";
@@ -7,12 +7,15 @@ import { handleEmailSignIn } from "@/app/lib/auth/emailSignInServerAction";
 
 const SignInPage: React.FC = () => {
   const [formData, setFormData] = useState({ email: "" as string });
+  const [isPending, startTransition] = useTransition();
 
   const handleSubmit = async (event: React.FormEvent) => {
     console.log("submitting form: ", formData);
     event.preventDefault();
     try {
-      await handleEmailSignIn(formData.email);
+      startTransition(async () => {
+        await handleEmailSignIn(formData.email);
+      });
     } catch (err) {
       console.error(err);
     }
@@ -20,23 +23,33 @@ const SignInPage: React.FC = () => {
   return (
     <div className="signin-page">
       <div className="signin-card">
-        <h2>Sign In</h2>
+        <h2 className="text-black">Sign In</h2>
         <div className="form-container">
           <form className="email-signin-form" onSubmit={handleSubmit}>
             <input
-              className="form-input"
+              className="form-input text-black"
+              style={{ backgroundColor: "white" }}
               type="email"
               maxLength={320}
               placeholder="Email Address"
               onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
                 setFormData({ email: event.target.value });
               }}
+              disabled={isPending}
+              required
             />
-            <button type="submit">Sign in via email</button>
+            <button type="submit" className="submit-button">
+              Sign in with email
+            </button>
           </form>
+          <div className="divider">
+            <div className="line"></div>
+            <span className="or">or</span>
+            <div className="line"></div>
+          </div>
           <div className="social-logins">
             <button
-              className="google"
+              className="google text-black no-border"
               onClick={() => {
                 handleGoogleSignIn();
               }}
