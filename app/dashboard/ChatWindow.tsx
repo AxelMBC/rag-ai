40f181e59b70b40aab87bdb72deeb081d372d811
3ARR4 <barrazaaxel2000@gmail.com>
2025-01-09 12:42:54 -0600

    file rename

--- app/dashboard/ChatWindow.tsx
+++ app/dashboard/ChatWindow.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import SideBar from "./sidebar/Header";
+import SideBar from "./sidebar";
 import Message from "./components/Message";
 import Loader from "../utils/Loader";
 import PromptInput from "./components/PromptInput";
