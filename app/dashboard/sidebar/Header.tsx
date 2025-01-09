94b3614c4b7bc9ed094f90690bc880408015b1cb
3ARR4 <barrazaaxel2000@gmail.com>
2025-01-08 19:01:07 -0600

    model selection filtered models to only text models

--- app/dashboard/sidebar/Header.tsx
+++ app/dashboard/sidebar/Header.tsx
@@ -105,11 +105,13 @@ const SideBar = ({ selectedModel, setSelectedModel }: ModelSelectorProps) => {
           >
             {selectedModel || "Select a Model"}
             {groqModels.map((model) => {
-              return (
-                <option key={model.id} value={model.id}>
-                  {model.id}
-                </option>
-              );
+              if (model.type === "text") {
+                return (
+                  <option key={model.id} value={model.id}>
+                    {model.id}
+                  </option>
+                );
+              }
             })}
           </select>
           {/* Footer section at the bottom */}
