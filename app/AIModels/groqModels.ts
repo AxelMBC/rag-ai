e2499eaa7a28488872478b940210eac628b06674
3ARR4 <barrazaaxel2000@gmail.com>
2025-01-11 21:57:34 -0600

    oopenai api working

--- app/AIModels/groqModels.ts
+++ app/AIModels/groqModels.ts
@@ -1,73 +1,55 @@
 export const groqModels = [
   {
-    id: "distil-whisper-large-v3-en",
-    owned_by: "HuggingFace",
-    type: "transcription",
-    description:
-      "Accelerated, accurate English speech recognition and transcription.",
-  },
-  {
-    id: "gemma2-9b-it",
-    owned_by: "Google",
-    type: "text",
-    description:
-      "Gemma-2-9B-IT excels in text generation, chatbots, and summarization.",
-  },
-  {
-    id: "gemma-7b-it",
-    owned_by: "Google",
-    type: "text",
-    description: "Google's open 7B model for versatile text generation.",
-  },
-  {
-    id: "llama-3.1-70b-versatile",
-    owned_by: "Meta",
-    type: "text",
-    description:
-      "Excels in multilingual text generation, coding, and reasoning.",
-  },
-  {
-    id: "llama-3.1-8b-instant",
-    owned_by: "Meta",
-    type: "text",
-    description: "Multilingual model for text, coding, and conversation.",
-  },
-  {
-    id: "llama-guard-3-8b",
-    owned_by: "Meta",
-    type: "security",
-    description: "Fine-tuned for content safety in LLM inputs and outputs.",
-  },
-  {
-    id: "llama3-70b-8192",
-    owned_by: "Meta",
-    type: "text",
-    description: "Meta's 70B parameter model with 8,192-token context.",
-  },
-  {
-    id: "llama3-8b-8192",
-    owned_by: "Meta",
-    type: "text",
-    description: "Meta's 8B parameter model with 8,192-token context.",
-  },
-  {
-    id: "mixtral-8x7b-32768",
-    owned_by: "Mistral",
-    type: "text",
-    description: "Sparse Mixture-of-Experts model with 32k token context",
-  },
-  {
-    id: "whisper-large-v3",
+    id: "chatgpt-4o-latest",
     owned_by: "OpenAI",
-    type: "transcription",
-    description:
-      "OpenAI's multilingual ASR (Automatic Speech Recognition) model, trained on extensive audio.",
+    type: "text",
+    description: "OpenAI's versatile, high-intelligence flagship model",
   },
   {
-    id: "whisper-large-v3-turbo",
+    id: "gpt-4o-mini",
     owned_by: "OpenAI",
-    type: "transcription",
-    description: "Accelerated multilingual ASR with minimal accuracy loss.",
+    type: "text",
+    description: "OpenAI's fast, affordable small model for focused tasks",
+  },
+  {
+    id: "gpt-4-turbo",
+    owned_by: "OpenAI",
+    type: "text",
+    description: "OpenAI's fast, affordable small model for focused tasks",
+  },
+  {
+    id: "o1",
+    owned_by: "OpenAI",
+    type: "text",
+    description: "Reasoning models that excel at complex, multi-step tasks",
+  },
+  {
+    id: "o1-mini",
+    owned_by: "OpenAI",
+    type: "text",
+    description:
+      "Mini Reasoning models that excel at complex, multi-step tasks",
+  },
+  {
+    id: "gpt-4o-realtime-preview",
+    owned_by: "OpenAI",
+    type: "text",
+    description:
+      "GPT-4o model capable of realtime text and audio inputs and outputs",
+  },
+  {
+    id: "gpt-4o-mini-realtime-preview",
+    owned_by: "OpenAI",
+    type: "text",
+    description:
+      "GPT-4o mini model capable of realtime text and audio inputs and outputs",
+  },
+  {
+    id: "gpt-4o-audio-preview",
+    owned_by: "OpenAI",
+    type: "audio",
+    description:
+      "GPT-4o models capable of audio inputs and outputs via REST API",
   },
 ];
 
--- app/api/inquiry/route.ts
+++ app/api/inquiry/route.ts
@@ -1,6 +1,7 @@
 import Groq from "groq-sdk";
+import OpenAI from "openai";
 
-const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+// const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 interface RequestProps {
   json():
@@ -9,11 +10,21 @@ interface RequestProps {
 }
 
 export async function POST(request: RequestProps) {
+  const openai = new OpenAI({
+    apiKey: process.env.GROQ_API_KEY,
+  });
+
   const { inquiry, model } = await request.json();
-  const response = await groq.chat.completions.create({
+  // const response = await groq.chat.completions.create({
+  //   messages: [{ role: "user", content: inquiry }],
+  //   model,
+  // });
+
+  const response = await openai.chat.completions.create({
     messages: [{ role: "user", content: inquiry }],
     model,
   });
+  console.log("resonse: ", response);
 
   return new Response(JSON.stringify({ message: "Groq Response", response }), {
     status: 200,
