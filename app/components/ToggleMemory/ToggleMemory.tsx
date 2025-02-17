095a3a57aef701c9964e83221e6fa335c69a1062
3ARR4 <barrazaaxel2000@gmail.com>
2025-02-17 14:55:30 -0600

    y

---
+++ app/components/ToggleMemory/ToggleMemory.tsx
@@ -0,0 +1,23 @@
+import "./toggleStyle.scss";
+interface ToggleType {
+  conversationalMemory: boolean;
+  setConversationalMemory: (value: boolean) => void;
+}
+
+const ToggleMemory = ({
+  conversationalMemory,
+  setConversationalMemory,
+}: ToggleType) => {
+  return (
+    <div className="toggle">
+      <input
+        type="checkbox"
+        id="btn"
+        onClick={() => setConversationalMemory(!conversationalMemory)}
+      />
+      <label htmlFor="btn"></label>
+    </div>
+  );
+};
+
+export default ToggleMemory;
